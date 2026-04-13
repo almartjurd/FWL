02e830bb193a4f4d051ea175477bebbe847a984b
almartjurd <tjeerdcorjanus@gmail.com>
2026-04-14 00:03:32 +0200

    v1.0
    Error Fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fwl-v1";
+const CACHE = "fwl-v2";
 const ASSETS = ["./", "./index.html"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
